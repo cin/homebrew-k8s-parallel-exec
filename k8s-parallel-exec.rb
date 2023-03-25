ebd89afbd971a2336dc76f26a9084ca9d5bd77be
GitHub Action <action@github.com>
2023-03-25 03:25:51 +0000

    Update k8s-parallel-exec to 0.0.9

--- k8s-parallel-exec.rb
+++ k8s-parallel-exec.rb
@@ -1,24 +1,24 @@
 class K8sParallelExec < Formula
   desc "Execute commands on Kubernetes pods in parallel"
   homepage "https://github.com/cin/k8s-parallel-exec"
-  version "0.0.8"
+  version "0.0.9"
   license "mit"
 
   if OS.mac?
     if Hardware::CPU.arm?
       url "https://github.com/cin/k8s-parallel-exec/releases/download/#{version}/k8s-parallel-exec-darwin-arm64.tgz"
-      sha256 "10f810e82f1bd925fe184f1931a839488714ec5aba06285cc09c3cb789fb825e"
+      sha256 "ab7b812bc0bdadc51030e5194bbf8cd94d68ce3ae27767eb345bf4f593e52e65"
     else
       url "https://github.com/cin/k8s-parallel-exec/releases/download/#{version}/k8s-parallel-exec-darwin-amd64.tgz"
-      sha256 "b421fcefe27d945042bc5d97aa74f53cdb69a19fad532f09f44e334d3966daa2"
+      sha256 "16c91d926e1246d9f53e755f3d4001f798d3b42c6f0b59a90e50ec56827909bb"
     end
   elsif OS.linux?
     if Hardware::CPU.arm?
       url "https://github.com/cin/k8s-parallel-exec/releases/download/#{version}/k8s-parallel-exec-linux-arm64.tgz"
-      sha256 "6d44d919dfa2dfdaef73b14669074e8bd579f68bc6ffe800be3abec89e3fb72c"
+      sha256 "58b2ff3be85147fcfa967a0716b58ba18948e882b7f7ef91e38bc529cb6db320"
     else
       url "https://github.com/cin/k8s-parallel-exec/releases/download/#{version}/k8s-parallel-exec-linux-amd64.tgz"
-      sha256 "d9e88f5d06e1b6c2d82d5c3e84940559a5ae068fbe07c92afd7c51ea668d48d5"
+      sha256 "0dca01695376fbf045c3fccf05d549fc7f3b494d843b18682918ac7422ce9cd5"
     end
   end
 
