05429673498a35f47e238bd88d0d1d1a818ca416
GitHub Action <action@github.com>
2023-03-25 06:09:40 +0000

    Update kubectl-parallel-exec to 0.0.12

--- kubectl-parallel-exec.rb
+++ kubectl-parallel-exec.rb
@@ -1,24 +1,24 @@
 class KubectlParallelExec < Formula
   desc "Execute commands on Kubernetes pods in parallel"
   homepage "https://github.com/cin/kubectl-parallel-exec"
-  version "0.0.11"
+  version "0.0.12"
   license "mit"
 
   if OS.mac?
     if Hardware::CPU.arm?
       url "https://github.com/cin/kubectl-parallel-exec/releases/download/#{version}/kubectl-parallel-exec-darwin-arm64.tgz"
-      sha256 "e7ffdc635f2089975d55e278e3347015dec3b3e3db023e60ab7b09666204e787"
+      sha256 "d152c34f474010147ae2fba632fe9d60379a5c5deaff2df1bbfa83b8a5bf1aee"
     else
       url "https://github.com/cin/kubectl-parallel-exec/releases/download/#{version}/kubectl-parallel-exec-darwin-amd64.tgz"
-      sha256 "2f0f7cd43e4f3065756fde62876d9733567fc49689f96895dd406bdc97473c74"
+      sha256 "c93ecc01340de940fd69f9abfecad4197da1dfedb76d52b12837f2cbcbfc3b43"
     end
   elsif OS.linux?
     if Hardware::CPU.arm?
       url "https://github.com/cin/kubectl-parallel-exec/releases/download/#{version}/kubectl-parallel-exec-linux-arm64.tgz"
-      sha256 "06bde9fdf90db8c3bae5446a298d19546b23689411cd51ddd4e84df97c433cca"
+      sha256 "ffc10f967c62887557485554b9fba4fbf5e088a2c2bf8c6342a47f5cd6ec0b17"
     else
       url "https://github.com/cin/kubectl-parallel-exec/releases/download/#{version}/kubectl-parallel-exec-linux-amd64.tgz"
-      sha256 "270edc09e111a40b9817b989c560ba89264d70130fd4fd283cf5ce997935f53c"
+      sha256 "88ee074adc8cf252a15afe6f268e545ca7725c58f2d4afbb2316f81911444d55"
     end
   end
 
